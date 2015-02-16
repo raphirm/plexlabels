5ac2b583fe60aa83b61dce94c7472db6e70fa6f1
raphirm <raphi.rm@gmail.com>
2015-02-16 15:31:49 +0100

    add endpoint for iterative update...

--- __main__.py
+++ __main__.py
@@ -7,12 +7,17 @@ if __name__ == "__main__":
     import sys;
     overviews = []
     conn = HttpConn(sys.argv[1], sys.argv[2], sys.argv[3])
+    update = sys.argv[4]
     intrusive = False
-    if len(sys.argv) >= 5:
-        if(sys.argv[4] == "intrusive"):
+    if len(sys.argv) >= 6:
+        if(sys.argv[5] == "intrusive"):
             intrusive = True
             print("make it intrusive")
+    
     if conn.testconnection():
-        overviews = conn.getoverview();
+        if(sys.argv[4] == "new"):
+            overviews = conn.getnew();
+        if(sys.argv[4] == "all"):
+            overviews = conn.getoverview();
     for section in overviews:
         parseXML(section, conn, intrusive)
