c46d5fc5a93f5c276c7b42e0bb27efe5d243082a
raphirm <raphi.rm@gmail.com>
2015-02-16 15:38:29 +0100

    add endpoint for iterative update...

--- HttpApi/HttpApi.py
+++ HttpApi/HttpApi.py
@@ -27,7 +27,14 @@ class HttpConn:
             connection.request("GET", "/library/sections/" + section + "/all?" + self.token)
             sectionoverview.append(connection.getresponse().readall())
         return sectionoverview
-
+    def getnew(self):
+        sectionoverview = []
+        sections = self.sections.split(",")
+        for section in sections:
+            connection = http.client.HTTPConnection(self.host + ":32400")
+            connection.request("GET", "/library/sections/" + section + "/recentlyAdded?" + self.token)
+            sectionoverview.append(connection.getresponse().readnew())
+        return sectionoverview
     def updateItem(self, itemid, labels):
         connection = http.client.HTTPConnection(self.host + ":32400")
         connection.request("GET", itemid+"?"+self.token);
